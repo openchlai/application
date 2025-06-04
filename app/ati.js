d3bed32b680c3a8b46fe863870ec6f851f64a463
Joseph Kimani <joseph.kimani@bitz-itc.com>
2025-06-04 19:40:46 +0300

    pick latest aii result only

--- app/ati.js
+++ app/ati.js
@@ -351,7 +351,7 @@ function atis (o,k,ts)
 				p_ = pcoll[2].firstChild.firstChild.firstChild.childNodes[2]; // reload aii sidebar
 				p_.childNodes[0].checked = true;
 				p_.childNodes[1].innerHTML = "..."
-				url (p_.childNodes[1], "case_insights","messages",  ("?src_callid="+ch[ATI.CHAN_BRIDGE_ID]+"&_c=30&sort=id")); // pick latest
+				url (p_.childNodes[1], "case_insights","messages",  ("?src_callid="+ch[ATI.CHAN_BRIDGE_ID]+"&_c=1&sort=id")); // pick latest
 			}
 		}
 	}
