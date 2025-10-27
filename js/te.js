6f9e1e79605b1019f4511e3de405db717e29e6b0
Joseph Kimani <joseph.kimani@bitz-itc.com>
2025-10-27 09:31:30 +0300

    network error message

--- js/te.js
+++ js/te.js
@@ -1245,9 +1245,13 @@ function url (p, u, m, a="", data=null, l=0, o=null, meth="GET")
         };
         x.onerror = function (error)
         {
-                console.error (error)
+               console.error (error)
+			var p = document.getElementById ("vv").childNodes[1];
+			p.parentNode.scroll ({ top: h, left: 0, behavior: 'smooth' });
+			p.innerHTML = "<div class='x15 yy gr'>Request failed due to Network Error.</div>";
         //      // window.alert ("Error: " + ev.target.status);
         };
+	   document.getElementById ("vv").childNodes[1].innerHTML = ""; // clear network error
         // todo: progress bar (useful for file upload)
         x.send (data);
 }
