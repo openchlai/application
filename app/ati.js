49516ac3a0f9e5ed55f78171015376752d619518
Joseph Kimani <joseph.kimani@bitz-itc.com>
2025-10-28 12:38:48 +0300

    ati popup

--- app/ati.js
+++ app/ati.js
@@ -114,11 +114,14 @@ te["ati_toolbar"] = { c:
 
 		{ div:["d w12 t01"], s:["abs w12 t17 b05 gw zzzz",""], c:
 		[
-			{ ac:["ay btn w09","","_ati_end","cb",""], c: 
+			{ div:["","ve"], c:
 			[
-				{ s:["c x t cb s","End Chat"] },
-				{ s:["c w03 h cb tc micon","last_page"] },
-				{ div:["e"] }
+				{ ac:["ay btn w09","","_ati_end","cb",""], c: 
+				[
+					{ s:["c x t cb s","End Chat"] },
+					{ s:["c w03 h cb tc micon","last_page"] },
+					{ div:["e"] }
+				]}
 			]}
 		]}, 
 
